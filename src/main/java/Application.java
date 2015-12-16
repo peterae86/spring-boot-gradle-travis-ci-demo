fb0665bef51e5b7f42d8747c34174fd6b4d5f9a4
peterae86 <peter26983045>
2015-12-16 21:23:50 +0800

    tmp

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by test on 2015/12/14.
+ */
+@Configurable
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
