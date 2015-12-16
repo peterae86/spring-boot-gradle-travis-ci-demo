e5786adc90d15a39abcdc47b4371f03f5cbf8434
peterae86 <peter26983045>
2015-12-16 23:23:10 +0800

    tmp

---
+++ src/main/java/com/test/config/Netty.java
@@ -0,0 +1,13 @@
+package com.test.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by test on 2015/12/16.
+ */
+@Configurable
+public class Netty {
+
+
+}
